c8b1c9781ce682f55bec44e1c3c8cdc3458f57aa
vidhanshu37 <kelawalavidhanshu@gmail.com>
2024-12-29 17:35:31 +0530

    locationAccess done

---
+++ lib/screens/add_manually_address/confirmLocation.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Confirmlocation extends StatefulWidget {
+  const Confirmlocation({super.key});
+
+  @override
+  State<Confirmlocation> createState() => _ConfirmlocationState();
+}
+
+class _ConfirmlocationState extends State<Confirmlocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("hello")),
+    );
+  }
+}
--- lib/screens/location_access/locationAccess.dart
+++ lib/screens/location_access/locationAccess.dart
@@ -6,6 +6,8 @@ import 'package:zesty/utils/constants/colors.dart';
 import 'package:zesty/utils/constants/media_query.dart';
 import 'package:zesty/utils/constants/text_string.dart';
 
+import '../add_manually_address/confirmLocation.dart';
+
 class LocationAccess extends StatefulWidget {
   const LocationAccess({super.key});
 
@@ -14,8 +16,8 @@ class LocationAccess extends StatefulWidget {
 }
 
 class _LocationAccessState extends State<LocationAccess> {
-
   var controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,15 +26,19 @@ class _LocationAccessState extends State<LocationAccess> {
         padding: const EdgeInsets.all(20.0),
         child: Stack(
           children: [
-
             /// current location button
             Positioned(
               bottom: 85,
               right: 0,
               left: 0,
               child: ZElevatedButton(
-                  title: "Use current location", onPress: () {
-              }),
+                  title: "Use current location",
+                  onPress: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Confirmlocation()));
+                  }),
             ),
 
             /// enter manually button
@@ -41,8 +47,12 @@ class _LocationAccessState extends State<LocationAccess> {
                 left: 0,
                 right: 0,
                 child: ZElevatedButton(
-                  bgColor: ZMediaQuery(context).isDarkMode ? TColors.darkerGrey : TColors.grey,
-                  btnTextColor: ZMediaQuery(context).isDarkMode ? TColors.bgLight : TColors.black,
+                  bgColor: ZMediaQuery(context).isDarkMode
+                      ? TColors.darkerGrey
+                      : TColors.grey,
+                  btnTextColor: ZMediaQuery(context).isDarkMode
+                      ? TColors.bgLight
+                      : TColors.black,
                   title: "Enter manually",
                   onPress: () {
                     showFloatingSheet(context);
@@ -51,61 +61,27 @@ class _LocationAccessState extends State<LocationAccess> {
 
             /// title text
             Positioned(
-              bottom: 230,
+                bottom: 230,
                 right: 0,
                 left: 0,
-                child: Center(child: Text(ZText.grantLocation, style: Theme.of(context).textTheme.headlineMedium))
-            ),
+                child: Center(
+                    child: Text(ZText.grantLocation,
+                        style: Theme.of(context).textTheme.headlineMedium))),
 
             /// description text
             Positioned(
                 bottom: 193,
                 right: 0,
                 left: 0,
-                child: Center(child: Text(ZText.locationAccessDesc, textAlign: TextAlign.center, style: Theme.of(context).textTheme.labelMedium,)))
-
+                child: Center(
+                    child: Text(
+                  ZText.locationAccessDesc,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.labelMedium,
+                )))
           ],
         ),
       )),
     );
   }
-
-
-  // Method to show the floating bottom sheet
-  void _showFloatingSheet(BuildContext context, TextEditingController controller, ) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true, // Makes the sheet floating
-      backgroundColor: Colors.transparent, // Make background transparent for styling
-      builder: (BuildContext context) {
-        return Container(
-          height: ZMediaQuery(context).height - 60,
-          width: ZMediaQuery(context).width,
-          decoration: BoxDecoration(
-            color: ZMediaQuery(context).isDarkMode ? TColors.bgDark : TColors.bgLight,
-            borderRadius: const BorderRadius.vertical(
-              top: Radius.circular(25),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                InkWell(onTap: () {
-                  Navigator.pop(context);
-                } ,child: Icon(Icons.arrow_back, size: 25,)),
-                SizedBox(height: 10,),
-                Text(ZText.searchLocation, style: Theme.of(context).textTheme.headlineMedium,),
-                SizedBox(height: 10,),
-                ZCustomTextField(controller: controller, hintText: "hintText")
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-
 }
