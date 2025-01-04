269032626e4b8ce9c942dcc1d770dc12d0ee0e05
vidhanshu37 <kelawalavidhanshu@gmail.com>
2025-01-04 12:26:53 +0530

    shimmerMap

---
+++ lib/screens/add_manually_address/shimmerMap.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:zesty/utils/constants/colors.dart';
+import 'package:zesty/utils/constants/media_query.dart';
+
+class ShimmerEffectsLocation extends StatelessWidget {
+
+  const ShimmerEffectsLocation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+     Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Shimmer.fromColors(
+            baseColor: ZMediaQuery(context).isDarkMode
+                ? TColors.darkerGrey
+                : TColors.grey,
+            highlightColor: ZMediaQuery(context).isDarkMode
+                ? TColors.darkGrey
+                : TColors.shimmerGrey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 15,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 10,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              ],
+            ),
+          ),
+        ],
+    );
+  }
+}
